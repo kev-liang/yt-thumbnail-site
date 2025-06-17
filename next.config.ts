11c0e2fa99de3ffd0c0b76016d00cafa7d383bc8
Micheal Yao <kevinman003@gmail.com>
2025-06-17 11:03:06 -0700

    Add build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/yt-thumbnail-site",
+  assetPrefix: "/yt-thumbnail-site",
 };
 
 export default nextConfig;
