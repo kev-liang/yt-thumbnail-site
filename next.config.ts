5e52ab0573a275adebf29207fd08b7d8ef9eb654
Micheal Yao <kevinman003@gmail.com>
2025-06-17 14:23:15 -0700

    Fix build

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/yt-thumbnail-site" : "",
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/yt-thumbnail-site" : "",
+  // basePath: process.env.NODE_ENV === "production" ? "/yt-thumbnail-site" : "",
+  // assetPrefix:
+  //   process.env.NODE_ENV === "production" ? "/yt-thumbnail-site" : "",
   images: {
     unoptimized: true,
   },
